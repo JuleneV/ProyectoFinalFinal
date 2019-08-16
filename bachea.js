8ec0aeda154176b301154d9d0420e6f02481b402
Julene Veerkamp <dorringen@hotmail.com>
2019-08-16 18:30:38 -0500

    add nuevos cambios bacheo

---
+++ bachea.js
@@ -0,0 +1,10 @@
+
+boton.addEventListener("click", cambiar)
+
+let input = document.getElementById("mostrarCorreo");
+let input = document.getElementById("mostratContraseña");
+       
+  
+
+    correo.innerHTML = "correo"
+    contraseñoa.innerHTML = "contraseña"
